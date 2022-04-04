f2188c013abf1ca47b4e59582738b591710b15a0
msomon <hsomon940@gmail.com>
2022-04-04 20:12:06 +0600

    add chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Dashboaed from './Component/Dashboard/Dashboaed';
+import Dashboard from './Component/Dashboard/Dashboard';
 import Reviews from './Component/Reviews/Reviews';
 import Home from './Component/Home/Home';
 import Navbar from './Component/Navbar/Navbar';
@@ -15,7 +15,7 @@ function App() {
     <Route path='/' element ={<Home></Home>} ></Route>
     <Route path='/home' element ={<Home></Home>} ></Route>
     <Route path='/reviews' element ={<Reviews></Reviews>} ></Route>
-    <Route path='/dashboard' element ={<Dashboaed></Dashboaed>} ></Route>
+    <Route path='/dashboard' element ={<Dashboard></Dashboard>} ></Route>
     <Route path='/blogs' element ={<Blogs></Blogs>} ></Route>
     <Route path='/' element ={<About></About>} ></Route>
 
---
+++ src/Component/Dashboard/Dashboard.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Line, LineChart } from 'recharts';
+
+import './Dashboard.css'
+
+const Dashboard = () => {
+  const data=[
+    {
+        "month": "Mar",
+        "investment": 100000,
+        "sell": 241,
+        "revenue": 10401
+    },
+    {
+        "month": "Apr",
+        "investment": 200000,
+        "sell": 423,
+        "revenue": 24500
+    },
+    {
+        "month": "May",
+        "investment": 500000,
+        "sell": 726,
+        "revenue": 67010
+    },
+    {
+        "month": "Jun",
+        "investment": 500000,
+        "sell": 529,
+        "revenue": 40405
+    },
+    {
+        "month": "Jul",
+        "investment": 600000,
+        "sell": 601,
+        "revenue": 50900
+    },
+    {
+        "month": "Aug",
+        "investment": 700000,
+        "sell": 670,
+        "revenue": 61000
+    }
+]
+  return (
+      
+    <div className='board'>
+         <div>
+         <h1>Month Wise Sell </h1>
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="sell" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+  
+      </div>
+      <div>
+        <h1>Investment VS Revenue</h1>
+      <LineChart
+      width={700}
+      height={500}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="month" />
+      <YAxis dataKey='investment' />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="investment" stroke="#8884d8" activeDot={{ r:8 }} />
+      <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+    </LineChart>
+      </div> 
+    </div>
+      
+   
+    
+  
+  );
+};
+
+export default Dashboard;
